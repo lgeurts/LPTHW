055e313b30b616dc971af3002fe209cd168be44b
Luc Geurts <lgeurts@protonmail.com>
2017-09-08 17:25:02 +0200

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -18,4 +18,3 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpol today."
 print "We need to put about", average_passengers_per_car, "in each car."
-
