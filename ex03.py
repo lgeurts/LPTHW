9ffdf168627f602dd108fc6d4602268469d34a1d
Luc Geurts <lgeurts@protonmail.com>
2017-11-09 02:47:20 +0100

    Add files via upload

--- ex03.py
+++ ex03.py
@@ -1,1 +1,30 @@
 # Exercise 3: Numbers and Math
+
+print("I will now count my chickens:")
+
+# Intro to PEMDAS rule: * and / always come first, + or - come second.
+
+# Example 1: / comes first, so 30 / 6 = 5. Then 25 + 5 = 30.
+print("Hens",  25 + 30 / 6)
+
+# Example 2: * comes first, so 25 * 3 = 75. Then 4 % of 75 = 3. 100 - 3 = 97.
+print("Roosters ", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# Example 3: (3 + 2 + 1 - 5) + (4 % 2) - (1 / 4) + 6.
+# Example 3 simplified: (1) + (0) - (0.25) + (6).
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's false.")
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= - 2)
+print("Is it less or equal?", 5 <= - 2)
